aaa22806968f1e92fcfb9833d8f060030703223c
YaminiAdduri <yamini.adduri12@gmail.com>
2023-04-03 12:10:35 +0530

    commit

---
+++ cmail.py
@@ -0,0 +1,24 @@
+import smtplib
+from smtplib import SMTP
+from email.message import EmailMessage
+def sendmail(to,subject,body):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('yamini.adduri12@gmail.com','lusrsqtmhettkejp')
+    msg=EmailMessage()
+    msg['From']='yamini.adduri12@gmail.com'
+    msg['Subject']=subject
+    msg['To']=to
+    msg.set_content(body)
+    server.send_message(msg)
+    server.quit()
+
+    
+
+
+
+
+
+
+
+        
+    
